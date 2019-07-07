b0fa483805e1688811f270898e67583460d10dfd
QiruiZhang0 <43439740+QiruiZhang0@users.noreply.github.com>
2019-07-07 01:31:05 +0100

    Add methods in Location System

--- src/Controller/Command/OpenDoorCommand.java
+++ src/Controller/Command/OpenDoorCommand.java
@@ -1,4 +1,11 @@
 package Controller.Command;
 
+import Controller.LocationController;
+
 public class OpenDoorCommand implements Command {
+    LocationController locationController = new LocationController();
+
+    public void excute(String keyId){
+        locationController.openDoor(keyId);
+    }
 }
--- src/Model/Location/Door.java
+++ src/Model/Location/Door.java
@@ -1,6 +1,16 @@
 package Model.Location;
 
 public class Door extends Tile {
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public Door(boolean isMovable, String terrain, int energyCost) {
         super(isMovable, terrain, energyCost);
     }
