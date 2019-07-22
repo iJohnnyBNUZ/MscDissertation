c0a530ee577652a154183ff006de79b42c3c0e4d
iJohnny <iJohnny0108@gmail.com>
2019-07-22 10:58:20 +0100

    override equals method of Coordinate Class

--- Common/src/main/java/Model/Location/Coordinate.java
+++ Common/src/main/java/Model/Location/Coordinate.java
@@ -1,6 +1,7 @@
 package Model.Location;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Coordinate implements Serializable {
     private int xPosition;
@@ -26,4 +27,23 @@ public class Coordinate implements Serializable {
     public void setyPosition(int yPosition) {
         this.yPosition = yPosition;
     }
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Coordinate){
+            Coordinate coordinate = (Coordinate) obj;
+            return coordinate.getxPostion() == this.getxPostion() && coordinate.getyPosition() == this.getyPosition();
+        }
+        else {
+            return false;
+        }
+    }
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = result * 31 + xPosition;
+        result = result * 31 + yPosition;
+
+        return result;
+    }
+
 }
